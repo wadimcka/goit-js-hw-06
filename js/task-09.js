005ca496a8383f2296e53d02962c0d311396ffae
Vadym Bobyrenko <wadimcka@gmail.com>
2023-06-25 18:39:19 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const buttonEl = document.querySelector(".change-color");
+const bodyEl = document.body;
+const spanEl = document.querySelector(".color");
+console.log(spanEl);
+
+buttonEl.addEventListener("click", onButtonClick);
+
+function onButtonClick(event) {
+  const randomHexColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomHexColor;
+  spanEl.textContent = randomHexColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
